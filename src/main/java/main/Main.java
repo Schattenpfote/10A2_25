d6779288da0f160b4ad67d51eb5ad9b069e7829f
guest-2f8ey2 <guest-2f8ey2@R1>
2026-01-20 12:00:06 +0100

    Arraylist für gegner angelegt, hr. Koch fragen wie man dieses generelle Programmproblem löst, wie man das mit den Ticks löst.

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -7,6 +7,8 @@ package main;
 
 import assets.actors.Enemy;
 import assets.actors.Tower;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -22,49 +24,74 @@ public class Main {
         System.out.println(p.getName());
         p.setName("fghjklö");
         System.out.println(p.getName());
-        Enemy fish = new Enemy(5,5,50,5,0,0,null,"Fish");
+        List<Enemy> Enemies = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+        Enemies.add(new Enemy(5,5,50,5,0,0,null,"Fish"));            
+        }
+        for (int i = 0; i < Enemies.size(); i++) {
+            System.out.println(Enemies.get(i).getName());    
+        }
         Enemy banana = new Enemy(8,3,85,7,0,0,null,"Banana");
         Timer timer = new Timer();
-    one[0] = fish;
         TimerTask timerTask = new TimerTask() {
             @Override
             public void run() {
             if(Tickloop==20){
                 Tickloop=1;
             }
-                for (int i = 0; i < Tickloop; i++) {
+                for (int i = 1; i < Tickloop; i++) {
                     if (Tickcount % i == 0) {
                     switch(i){
                         case 1:
-                        for (Enemy one1 : one) {
-                            one1.update();
-                        }
-
+                        break;
                         case 2: 
+                        break;
                         case 3:
                         banana.update();
-                        case 4:    
+                        break;
+                        case 4: 
+                        break;
                         case 5: 
-                        fish.update();
+                            for (int j = 0; j < Enemies.size(); j++) {
+                            Enemies.get(j).update();    
+                            }
+                        break;
                         case 6: 
+                        break;
                         case 7: 
+                        break;
                         case 8:   
                         for (Enemy eight1 : eight) {
                             eight1.update();
                         }
-
-                        case 9:    
+                        break;
+                        case 9:  
+                        break;
                         case 10: 
+                        break;    
                         case 11: 
-                        case 12:  
-                        case 13:    
+                        break;    
+                        case 12: 
+                        break;    
+                        case 13: 
+                        break;    
                         case 14: 
+                        break;    
                         case 15: 
+                        break;    
                         case 16: 
-                        case 17:    
+                        break;
+                        case 17: 
+                        break;
                         case 18: 
-                        case 19: 
-                        case 20:                             
+                        break;    
+                        case 19:
+                        break;
+                        case 20:  
+                        break;
+                        default: 
+                            System.out.println("Fehler bei ticks");
+                        break;
                     }   
                     }
     
